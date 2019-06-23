b4a7de36cdf490a797bf4dcb10ce6358fac9dc1c
misraelson <misraelson@gmail.com>
2019-06-22 22:06:54 -0700

    added content and styling css rules

--- src/App.js
+++ src/App.js
@@ -5,16 +5,42 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <nav className="App__nav">
+      
+      <nav className="Nav">
         <p>just breathing</p>
       </nav>
 
-      <header className="App__header">
-        <img src={logo} className="App__logo" alt="logo" />
+      <header className="Logo-box">
+        <img src={logo} className="logo" alt="logo" />
       </header>
-      
-      <footer className="App__footer">
-        2019 All Rights Reserved by Matthew Israelson 
+
+      <main className="Main">
+        <div className="Main__content-box">
+          <h1>About</h1>
+          <p>
+            You have the power to breathe! Take control over your autonomic nervous system through integrated breath-work. No complex user sign-ups, no push-notifications or annoying reminders, no social networking, no comparisons, JUST BREATHING.
+          </p>
+          <p>
+            Choose long, luxurious breaths to slow your heart rate, increase your VO2 max and enter into a state of deep relaxation and zen-like calm via Theta brain wave stimulation. Or choose a quicker breath pattern to stimulate the endocrine system and saturate the blood and brain with beautiful oxygen.
+          </p>
+          <p>
+            Focus on the breath ball to sync up your breaths per minute or close your eyes and listen to the soothing audio cues as you exercise your lungs and regain control over your prana.
+          </p>
+          <p>
+            Just Breathing is a free app for integrated breath-work and meditation. We hope you enjoy your breathing experience and we aim to add more breathing exercises and styles in future versions.
+          </p>
+          <p>
+            Be kind to everyone out there and enjoy the free air
+          </p>
+        </div>
+        <div className="Main__content-box">
+          <h1>Contact</h1>
+          <p>Any questions regarding the Just Breathing app please contact misraelson@gmail.com</p>
+        </div>
+      </main>
+
+      <footer className="Footer">
+        &copy;2019 All Rights Reserved by Matthew Israelson 
       </footer>
     </div>
   );
